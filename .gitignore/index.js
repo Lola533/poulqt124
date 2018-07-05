0e9ddc55c9be0390aed23fc6f83ae1b61f1b5a06
emile971 <37877869+emile971@users.noreply.github.com>
2018-07-05 19:03:57 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,44 @@
+const Discord = require ('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("z.")
+
+bot.on('ready', function() {
+    bot.user.setGame("aide : z.help");
+    console.log("le bot est bien connecter");
+});
+
+bot.login("NDI4NTcwOTU1OTMwMTQwNjc0.DZ6-bQ.hNsK3uc2PM0_nuSKjR3zj7H0Wy8");
+
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        var embed = new Discord.RichEmbed()
+                .setTitle("Commandes")
+                .addField("z.ip", "z.info", true)
+                .setColor("#2EFE2E")
+            message.channel.sendEmbed(embed)
+        }
+
+    if (message.content === prefix + "ip"){
+        var embed = new Discord.RichEmbed()
+            .setTitle("Ip")
+            .addField("Site", "Bientot", true)
+            .addField("Port", "Bientot", true)
+            .addField("Status", "Ouvert, Whitelist", true)
+            .setColor("#2EFE2E")
+        message.channel.sendEmbed(embed)
+    }
+
+    if (message.content === prefix + "info"){
+         var embed = new Discord.RichEmbed()
+             .setTitle("Info")
+             .setDescription("Reseaux")
+             .addField("Site", "https://zeldoria-network.jimdosite.com/", true)
+             .addField("Twitter", "https://mobile.twitter.com/ZeldoriaMCPE", true)
+             .addField("Youtube", "https://www.youtube.com/channel/UCwx3328ntu7rCS6N_f5oBFQ", true)
+             .setColor("0x0000FF")
+        message.channel.sendEmbed(embed);
+    }
+
+});
