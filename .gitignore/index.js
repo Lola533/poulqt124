d8e3a6ddc39f5c4c6ab46bffced536345baf7332
Spectateur_ <37877869+emile971@users.noreply.github.com>
2018-07-06 21:06:17 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,5 @@
 const Discord = require ('discord.js');
 const bot = new Discord.Client();
-const YTDL = require("ytdl-core");
 var prefix = ("z.")
 
 bot.on('ready', function() {
@@ -10,71 +9,8 @@ bot.on('ready', function() {
 
 bot.login(process.env.TOKEN);
 
-function play(connection, message) {
-      var server = servers[message.guild.id];
-
-      server.dispatcher = connection.playStream(YTDL(server.queue[0]), {filter: "audioonly"});
-
-      server.queue.shift();
-
-      server.dispatcher.on("end", function() {
-          if (server.queue[0])  play(connection, message);
-          else connection.disconnect();
-     });
-
-}
-
-var servers = {};
-
 bot.on('message', message => {
-    if (message.author.equals(bot.user)) return;
-
-    if(!message.content.startsWith(prefix)) return;
-
-    var args = message.substring(prefix.length).split(" ")
-
-    switch (args[0].toLowerCase()) {
-        case "play":
-            if(!args[1]) {
-               message.channel.sendMessage("Veuillez  mettre un lien youtube");
-               return;
-            }
-    
-            if (!message.member.voiceChannel) {
-                message.channel.sendMessage("Vous devez être dans un channel pour faire cette commande");
-                return;
-    
-    
-    
-        }
-    
-        if(!servers[message.guild.id]) servers[message.guild.id] = {
-        queue: []
-        };
-    
-    
-        var server = servers[message.guild.id];
-    
-        server.queue.push([1]);
-    
-        if(!message.member.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-        play(connection, message);
-        });
-        break;
-        case "skip":
-        var server = servers[message.guild.id];
-    
-        if (server.dispatcher) server.dispatcher.end();
-        break;
-        case "stop":
-        var server = servers[message.guild.id];
-        
-        if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-        break;
-        default:
-        message.channel.sendMessage("Erreur de commande, Réesayer")
-    };
-
+ 
     if (message.content === prefix + "help"){
         var embed = new Discord.RichEmbed()
                 .setTitle("Commandes")
