f31bfab47343b7858e438038b9b30ff4d88c9cae
Spectateur_ <37877869+emile971@users.noreply.github.com>
2018-07-07 20:07:47 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const YTDL = require("ytdl-core");
 
 var prefix = ("z.")
 
@@ -11,87 +10,20 @@ bot.on('ready', function() {
 
 bot.login(process.env.TOKEN)
 
-function play(connection, message) {
-      var server = servers[message.guild.id];
-
-      server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-
-      server.queue.shift();
-
-      server.dispatcher.on("end", function() {
-          if (server.queue[0])  play(connection, message);
-          else connection.disconnect();    
-     });
-
-    }
-
-
-
-var servers = {};
-
-bot.on('message', message => {
-    if (message.author.equals(bot.user)) return;
-
-    if(!message.content.startsWith(prefix)) return;
-
-    var args 
-    switch (args[0].toLowerCase()) {
-        case "play":
-            if(!args[1]) {
-               message.channel.sendMessage("Veuillez  mettre un lien youtube");
-               return;
-            }
-    
-            if (!message.member.voiceChannel) {
-                message.channel.sendMessage("Vous devez être dans un channel pour faire cette commande");
-                return;
-    
-    
-    
-        }
-    
-        if(!servers[message.guild.id]) servers[message.guild.id] = {
-        queue: []
-        }
-    
-    
-        var server = servers[message.guild.id];
-    
-        server.queue.push([1]);
-    
-        if(!message.member.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-        play(connection, message);
-        });
-        break;
-        case "skip":
-        var server = servers[message.guild.id];
-    
-        if (server.dispatcher) server.dispatcher.end();
-        break;
-        case "stop":
-        var server = servers[message.guild.id];
-        
-        if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-        break;
-        default:
-        message.channel.sendMessage("Erreur de commande, Réesayer")
-    }
-});
 
 bot.on('message', message => {
 
     if (message.content === prefix + "help"){
         var embed = new Discord.RichEmbed()
                 .setTitle("Commandes")
-                .addField("z.ip", "z.info", true)
+                .addField("z.ip", "z.info", "mute/demute" true)
                 .setColor("#2EFE2E")
             message.channel.sendEmbed(embed)
         }
 
     if (message.content === prefix + "ip"){
-        var embed = new Discord.RichEmbed()
-            .setTitle("Ip")
-            .addField("", "Bientot", true)
+        var embed = new Discord.RichEmbed() 
+            .addField("Ip", "Bientot", true)
             .addField("Port", "Bientot", true)
             .addField("Status", "Ouvert, Whitelist", true)
             .setColor("#2EFE2E")
@@ -102,11 +34,46 @@ bot.on('message', message => {
          var embed = new Discord.RichEmbed()
              .setTitle("Info")
              .setDescription("Reseaux")
-             .addField("Site", "https://zeldoria-network.jimdosite.com/", true)
+             .addField("Site", "En Dev", true)
              .addField("Twitter", "https://mobile.twitter.com/ZeldoriaMCPE", true)
              .addField("Youtube", "https://www.youtube.com/channel/UCwx3328ntu7rCS6N_f5oBFQ", true)
              .setColor("0x0000FF")
         message.channel.sendEmbed(embed);
     }
+    if (message.content.startsWith(prefix + "mute")) {
+     if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.sendMessage("Vous n'avez pas les permission")
+
+     if(message.mentions.users.size === 0) {
+         return message.channel.send("Vous devez mentionner un Utilisateur")
+     }
+
+     var mute = message.guild.member(message.mentions.users.first());
+     if(!mute){
+         return message.channel.send("Je n'ai pas trouver l'utilisateur ou il n'existe pas")
+     }
+
+     if(!message.guild.member(bot.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas les permission requise pour mute cette utilisateur");
+     message.channel.overwritePermissions(mute, {SEND_MESSAGES: false}).then(member => {
+         message.channel.send('${mute.user.username} est mute');
+     })
+    }
+
+    if (message.content.startsWith(prefix + "demute")) {
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.sendMessage("Vous n'avez pas les permission")
    
-});
+        if(message.mentions.users.size === 0) {
+            return message.channel.send("Vous devez mentionner un Utilisateur")
+        }
+   
+        var mute = message.guild.member(message.mentions.users.first());
+        if(!mute){
+            return message.channel.send("Je n'ai pas trouver l'utilisateur ou il n'existe pas")
+        }
+   
+        if(!message.guild.member(bot.user).hasPermission("ADMINISTRATOR")) return message.channel.send("Je n'ai pas les permission requise pour mute cette utilisateur");
+        message.channel.overwritePermissions(mute, {SEND_MESSAGES: true}).then(member => {
+            message.channel.send('${mute.user.username} est unmute');
+        })
+       }
+      
+   });
