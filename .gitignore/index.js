bc3657fb7f92de0a46d01a23024b488a149d67af
Spectateur_ <37877869+emile971@users.noreply.github.com>
2018-07-12 18:48:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -34,6 +34,7 @@ bot.on('message', message => {
     
     if ( message . content  === préfixe +  "adaptemonskin" ) {
         message.reply("Heum ca va etre compliquer louuuuu")
+    }
 
     if (message.content === prefix + "ping"){
        message.channel.sendMessage('Syncronisation avec le serveur: `' + `${message.createdTimestamp - Date.now()}` + ' ms`');
