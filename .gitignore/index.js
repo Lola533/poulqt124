c5bb84d5e1fd8885968bfa0efeb514f246aa8892
Spectateur_ <37877869+emile971@users.noreply.github.com>
2018-10-09 19:15:29 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -520,7 +520,7 @@ bot.on("guildCreate", guild => {
       .setFooter(`https://discord.gg/${invite.code}`)
 .setColor('#320242');
  
-bot.channels.find('id',"490087939754426378").send(serverembed);
+bot.channels.find('id',"498419215980822538").send(serverembed);
  
  
 })
