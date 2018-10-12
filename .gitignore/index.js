16c9565f32a1f538178aa63b802a7f3a02e7ee79
Spectateur_ <37877869+emile971@users.noreply.github.com>
2018-10-12 18:44:32 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -531,7 +531,7 @@ var embed = new Discord.RichEmbed()
 .setTitle("Nouvel Utilisateur!")
 .setColor("#320242")
 .addField(`Bienvenue a ${member.user.username}`)
-  member.guild.channel.find("name", "bienvenue").send(embed);
+  member.guild.channels.find("name", "bienvenue").send(embed);
 
 });
 
